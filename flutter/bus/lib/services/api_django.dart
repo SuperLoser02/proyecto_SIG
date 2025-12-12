ad5a8616769858d84a1098d9738fc0fcb0656e9a
Fabio Alejandro Arnez Fernandez <fabioarnez2002f@gmail.com>
2025-12-11 21:47:43 -0400

    imagenes agregados

--- flutter/bus/lib/services/api_django.dart
+++ flutter/bus/lib/services/api_django.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 class ApiService {
   // Cambia esta URL por la de tu servidor
   static const String baseUrl = 'http://10.0.2.2:8000/api';  // O tu IP/dominio
+  // static const String baseUrl = 'http://localhost:8000/api';
 
   static Future<Map<String, dynamic>> getAllData() async {
     try {
